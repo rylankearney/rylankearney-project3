b0e9d8d2a75e064567c0275f768d5f5c742adce9
rylankearney <rylan.isabel@live.ca>
2022-12-02 18:32:06 -0500

    comments are appearing on the page & some styling started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,102 @@
 import './App.css';
+import app from './firebase.js';
+import { useState, useEffect } from 'react';
+import { getDatabase, ref, onValue, push, remove } from 'firebase/database';
+import React from 'react';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons'
+import Footer from './Footer.js';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to the DEV branch</h1>
-    </div>
-  );
-}
+
+
+  const [ comments, setComments] = useState( [] );
+
+  const [userInput, setUserInput] = useState('');
+
+
+  useEffect( () => {
+    const database = getDatabase(app);
+    const dbRef = ref(database);
+
+    onValue(dbRef, (resp) => {
+      //console.log(resp.val());
+
+      const data = resp.val()
+
+      const updatedDatabaseInfo = [];
+      for (let key in data) {
+        //console.log(key)
+        //console.log(data[key])
+        updatedDatabaseInfo.push({key: key, name: data[key]});
+      }
+      //Passing that array INTO our setComments function to update our stateful variable
+
+      setComments(updatedDatabaseInfo);
+    })
+  }, [])
+
+  const handleInputChange = (e) =>{
+    setUserInput(e.target.value)
+    
+  }
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+  
+
+
+  const database = getDatabase(app);
+  const dbRef = ref(database);
+
+  push(dbRef, userInput);
+
+  setUserInput('');
+  }
+
+  const handleRemoveComment = (commentId) => {
+    const database = getDatabase(app);
+    const dbRef = ref(database, `/${commentId}`)
+
+    remove(dbRef);
+  }
+
+      return (
+        <div className="App wrapper">
+          <h1>Got a customer complaint?</h1>
+          <ul className="rules">
+            <li>No slurs/namecalling (Karen is an exception)</li>
+            <li>Keep it PG</li>
+            <li>Keep it anonymous</li>
+          </ul>
+            <form action="submit">
+              <label htmlFor="newComment">Spill the tea below</label>
+
+
+
+              <textarea type="text" id="newComment" onChange={handleInputChange} value={userInput} />
+              <button className="postButton" onClick={handleSubmit}>Post</button>
+            </form>
+            <ul id="displayedComments">
+              {comments.map((comments) => {
+                return (
+              
+              <li key={comments.key}>
+                <p>{comments.name}</p>
+                <button 
+                className="closeButton"
+                onClick={ () => handleRemoveComment(comments.key) }>
+                  <FontAwesomeIcon icon={faX} />
+                </button>
+              </li>
+              )
+
+              })}
+            </ul>
+            <Footer />
+        </div>
+      );
+    }
+
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<p>Created at <a href="">Juno College of Technology</a> by <a href="">Rylan Kearney</a></p>
+	)
+}
+
+export default Footer
