1c1523c4f895958ff0ebe51f4248cb5bed3d39b7
rylankearney <rylan.isabel@live.ca>
2022-12-03 19:06:32 -0500

    styling mostly finished & header image added

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,13 @@
 import React from "react"
+import Karen from "./Karen1.jpeg"
 
 const Header = () => {
 	return (
 		<>
+		<div className="headerFlex">
 			<h1>Welp.</h1>
+			<img src={Karen} alt="Drawing of a Karen looking upset" />
+		</div>
 			<h2>Got a customer complaint?</h2>
 		</>
 	)
--- src/Rules.js
+++ src/Rules.js
@@ -3,7 +3,7 @@ import React from "react"
 const Rules = () => {
 	return (
 		<>
-		<p>Are you a service worker with a cringeworthy or funny story about a customer interaction? Share it here!</p>
+		<p className="descText">Are you a service worker with a cringeworthy or funny story about a customer interaction? Share it here!</p>
 		<ul className="rules">
 			<h3>House Rules:</h3>
 			<li>No slurs/namecalling (Karen is OK)</li>
